305a288bdcad4074036010006d2e2d0f0b678acb
Alexander Melihov <amelihovv@ya.ru>
2017-08-31 10:16:47 +0300

    Update laravel-env-validator.php

--- src/config/laravel-env-validator.php
+++ src/config/laravel-env-validator.php
@@ -3,7 +3,30 @@
 return [
     'live_validation' => false,
     'rules' => [
-        // 'SOME_IMPORTANT_VARIABLE' => 'required',
-        // 'ANOTHER_IMPORTANT_ONE'   => 'required|in:TYPE_A,TYPE_B,TYPE_C',
+        'APP_ENV' => 'in:local,production',
+        'APP_KEY' => 'required',
+        'APP_DEBUG' => 'boolean',
+        
+        'DB_CONNECTION' => 'required',
+        'DB_HOST' => 'required',
+        'DB_PORT' => 'required',
+        'DB_DATABASE' => 'required',
+        'DB_USERNAME' => 'required',
+        'DB_PASSWORD' => 'present',
+        
+        'MAIL_HOST' => 'required',
+        'MAIL_PORT' => 'required',
+        'MAIL_USERNAME' => 'required',
+        'MAIL_PASSWORD' => 'required',
+        'MAIL_ENCRYPTION' => 'nullable',
+        'MAIL_FROM_ADDRESS' => 'required|email',
+        'MAIL_FROM_NAME' => 'required',
+        
+        'BROADCAST_DRIVER' => 'nullable|in:pusher,redis,log,null',
+        'CACHE_DRIVER' => 'in:apc,array,database,file,memcached,redis',
+        'SESSION_DRIVER' => 'in:file,cookie,database,apc,memcached,redis,array',
+        'QUEUE_DRIVER' => 'in:sync,database,beanstalkd,sqs,redis,null',
+        'MAIL_DRIVER' => 'in:smtp,sendmail,mailgun,mandrill,ses,' .
+            'sparkpost,log,array',
     ],
 ];
